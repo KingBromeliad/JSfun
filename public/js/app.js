01b3e2fb5a452794799c26f00a2e9da9d810449d
KingBromeliad2 <busi.samuele99@gmail.com>
2021-02-23 15:45:28 +0100

    socketio included

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,27 @@
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
+var socket = io('http://socketserver.com:1923');
 
+
+// Vue use modules
+Vue.use(VueSocketIOExt, socket);
+
+/****************************** 
+            ROUTES
+ ******************************/
 const routes = [
     { path: '/foo', component: Foo },
     { path: '/bar', component: Bar }
   ]
 
+// Router
   const router = new VueRouter({
     routes // short for `routes: routes`
   })
 
-  
+/****************************** 
+             COMPONENTS
+ ******************************/
 
 Vue.component('button-counter', {
     data: function () {
@@ -24,4 +35,3 @@ Vue.component('button-counter', {
 const app = new Vue({
     router
   }).$mount('#app')
-  
